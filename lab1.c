ecca7607820068a0e7c18eee9d8a874e4830f063
Matt Hixon <mhixon333@sbcglobal.net>
2016-02-01 20:29:16 -0600

    lab1 initial commit

---
+++ lab1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("----- Vigenere Cipher -----\n---- Matt Hixon CIS308 ----\n");
+	
+	printf("\nEnter a keyword: ");
+	char *keyword;
+	scanf("%c", &keyword);
+	printf("\nYour keyword is: %c", keyword);
+
+	//once you have keyword, take first letter of keyword and add first letter of word to encode %26 
+
+
+
+
+}
