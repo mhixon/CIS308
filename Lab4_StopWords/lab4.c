700832dc924c422ce4b05750ea4152e0ecd9f489
Matt Hixon <mhixon@contour.cis.ksu.edu>
2016-03-22 23:39:37 -0500

    integrate tree attempt

--- Lab4_StopWords/lab4.c
+++ Lab4_StopWords/lab4.c
@@ -6,6 +6,71 @@
 #include<dirent.h>
 #include<errno.h>
 #include<stdlib.h>
+#
+
+//used tree template from http://www.cprogramming.com/tutorial/c/lesson18.html
+struct node{
+	char* word;
+	char** languages;
+	struct node *left;
+	struct node *right;
+};
+
+struct node *root = 0;
+
+
+void destroy_tree(struct node *leaf)
+{
+  if( leaf != 0 )
+  {
+      destroy_tree(leaf->left);
+      destroy_tree(leaf->right);
+      free( leaf );
+  }
+}
+
+
+void insert(char[] key, struct node **leaf)
+{
+    if( *leaf == 0 )
+    {
+        *leaf = (struct node*) malloc( sizeof( struct node ) );
+        (*leaf)->word = key;
+        /* initialize the children to null */
+        (*leaf)->left = 0;    
+        (*leaf)->right = 0;  
+    }
+    else if(strcmp(key, (*leaf)->word) < 0)
+    {
+        insert( key, &(*leaf)->left );
+    }
+    else if(strcmp(key, (*leaf)->word) > 0)
+    {
+        insert( key, &(*leaf)->right );
+    }
+}
+
+struct node *search(int key, struct node *leaf)
+{
+  if( leaf != 0 )
+  {
+	
+      if(strcmp(key, leaf->word) == 0)
+      {
+          return leaf;
+      }
+      else if(strcmp(key, leaf->word) < 0)
+      {
+          return search(key, leaf->left);
+      }
+      else
+      {
+          return search(key, leaf->right);
+      }
+  }
+  else return 0;
+}
+
 
 void readDirectory(char* dir) {
 	//check if dir exists
@@ -31,6 +96,19 @@ void readDirectory(char* dir) {
 			//char*[] languages
 			//tree leftchild
 			//tree rightchild
+			char line[300];
+			FILE* fp = fopen(dp->d_name, "r");
+			if(fp == NULL){
+				perror("[ERROR]");
+				return;			
+			} else {
+				while(fgets(line, 300, fp) != NULL){
+					struct *node newNode = (struct node)malloc(sizeof(struct node));
+					newNode->word = line;
+					newNode->languages = dp->d_name;
+					insert(dp->d_name, newNode);
+				}
+			}
 			printf("%s\n", dp->d_name);
         }
     } while (dp != NULL);
