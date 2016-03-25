eafe6c419d898cf0139152a3f160c6ec43a478c5
Matt Hixon <mhixon333@sbcglobal.net>
2016-03-25 18:52:53 -0500

    lab4 complete

--- Lab4_StopWords/lab4.c
+++ Lab4_StopWords/lab4.c
@@ -10,12 +10,14 @@
 #include<ctype.h>
 #include<stdlib.h>
 
-
-
 #define MAX 32
 
-//used tree template from http://www.cprogramming.com/tutorial/c/lesson18.html
-struct node{
+
+//started with tree template from http://www.cprogramming.com/tutorial/c/lesson18.html
+ 
+
+struct node
+{
 	char* word;
 	char languages[MAX][64];
 	int languageIndex;
@@ -23,129 +25,121 @@ struct node{
 	struct node *right;
 };
 
-struct node *root = 0;
 
-
-void destroy_tree(struct node *leaf)
+void destroy_tree(struct node **leaf)
 {
-  if( leaf != 0 )
+  if( *leaf != NULL )
   {
-      destroy_tree(leaf->left);
-      destroy_tree(leaf->right);
-      free( leaf );
+      destroy_tree(&(*leaf)->left);
+      destroy_tree(&(*leaf)->right);
+      free((*leaf)->word);
+      free((*leaf));
   }
 }
 
 
 void insert(char* key, struct node **leaf, char* lang)
 {
-	//puts("Insert Method");
+
     if( *leaf == NULL )
     {
-    	//puts("NULL LEAF");
         *leaf = (struct node*) malloc( sizeof(struct node));
-        (*leaf)->word = malloc(strlen (key) + 1);
-        strcpy ((*leaf)->word, key);
-        //puts("LEAF CPY");
+
+        (*leaf)->word = strdup(key);
+        lang = strtok(lang, ".");
         memset((*leaf)->languages, 0, sizeof((*leaf)->languages));
         strcpy ((*leaf)->languages[0],lang);
         (*leaf)->languageIndex = 1;
-        /* initialize the children to null */
         (*leaf)->left = NULL;    
-        (*leaf)->right = NULL;
-        printf("Key: %s,  Value: %s,  Lang1: %s\n" , key, (*leaf)->word, (*leaf)->languages[0]);  
+        (*leaf)->right = NULL; 
     }
-    else if(strcmp(key, (*leaf)->word) < 0)
+    else if(strcmp(key, (*leaf)->word) < 0) { insert( key, &(*leaf)->left, lang); }
+    else if(strcmp(key, (*leaf)->word) > 0) { insert( key, &(*leaf)->right, lang); } 
+    else 
     {
-        insert( key, &(*leaf)->left, lang);
-    }
-    else if(strcmp(key, (*leaf)->word) > 0)
-    {
-        insert( key, &(*leaf)->right, lang);
-    } else {
     	strcpy ((*leaf)->languages[(*leaf)->languageIndex], lang);
     	(*leaf)->languageIndex += 1;
     }
 }
 
-struct node *search(char* key, struct node *leaf)
+
+void search(char* key, struct node *leaf)
 {
-  if( leaf != 0 )
+  if( leaf != NULL )
   {
-	
       if(strcmp(key, leaf->word) == 0)
       {
-          return leaf;
+      	int num = leaf->languageIndex;
+		int i;
+
+		printf("WORD : %s\n", leaf->word);
+		for(i = 0; i < num; i++){
+			printf("LANGUAGE %d: %s\n", i+1 ,leaf->languages[(i)]);
+		}
       }
-      else if(strcmp(key, leaf->word) < 0)
-      {
-          return search(key, leaf->left);
-      }
-      else
-      {
-          return search(key, leaf->right);
-      }
-  }
-  else return 0;
+      else if(strcmp(key, leaf->word) < 0) { return search(key, leaf->left); }
+
+      else { return search(key, leaf->right); }
+  
+  }else{ puts("Word not found"); }
 }
 
 
-
-
-void readDirectory(char* dir, struct node* root) {
+void readDirectory(char* dir, struct node** rootp) {
 	//check if dir exists
 	DIR *dirp = opendir(dir);
-	if(dirp == NULL){
-		perror("couldn't open");
-		return;
-	} else{
+	if(dirp == NULL){ perror("couldn't open"); } 
+	else
+	{
+		struct dirent *dp = readdir(dirp);
+		char* fileName = strcat(dir, "/");
+		char lng[64];
+		while((dp = readdir(dirp)) != NULL){
+			char line[300];
+			strcpy(lng, fileName);
+			strcat(lng, dp->d_name);
 
+			FILE* fp = fopen(lng, "r");
+			if(fp == NULL){ perror("[ERROR]"); } 
+			else {
+				while(fgets(line, 300, fp) != NULL){
 
-	
-	//store everything in dir
-	struct dirent *dp = readdir(dirp);
-	char* fileName = strcat(dir, "/");
-	char lng[64];
-	//printf("FILENAME: %s\n", fileName);
-	
-	//iterate through dir
+					int slen = strlen(line);
+					while(isspace(line[slen-1]) && slen){ slen--; }
 
-	while((dp = readdir(dirp)) != NULL){
-		char line[300];
-		//printf("%s\n", dp->d_name);
-		strcpy(lng, fileName);
-		strcat(lng, dp->d_name);
-		printf("%s\n", lng);
-		FILE* fp = fopen(lng, "r");
-		if(fp == NULL){
-			perror("[ERROR]");
-			return;			
-		} else {
-			while(fgets(line, 300, fp) != NULL){
-				insert(line, &root, dp->d_name);
+					if(slen){ line[slen] = '\0'; }
+					insert(line, rootp, dp->d_name);
+				}
 			}
 		}
+		closedir(dirp);
 	}
-	closedir(dirp);
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
 	puts("\n\n ----- 	Lab 4: Stop Words 	----- ");
 	puts(" -----	Author: Matt Hixon 	-----\n\n");
 
-	puts("Enter the name of a directory containing stop words: \n");
-	
-	char* stopWordDir = (char*)malloc(sizeof(256));
-	scanf("%s", stopWordDir);
 	struct node *root = NULL;
-	readDirectory(stopWordDir, root);
-	struct node *result = search("ale", root);
-	printf("RESULT: %s", result->word);
-	
+	//check if directory was entered
+	if(argc != 2 || (opendir(argv[1])) == NULL)
+	{
+		puts("Command line argument error.\n");
+		puts("Please re-run program with name of valid directory to read from.");
 
+	} 
+	else 
+	{
+		printf("Collecting words from %s\n", argv[1]);
+		readDirectory(argv[1], &root);
+		char* wordToFind = (char*)malloc(sizeof(256));
+		puts("Enter Word to find: \n");
+		scanf("%s", wordToFind);
+		search(wordToFind, root);
+	}
 
-	//printf("%s", stopWordDir);
+	destroy_tree(&root);
+	root = NULL;
 	return 0;
-
 }
