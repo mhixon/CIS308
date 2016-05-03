e9036b91b47f6d4947f84e73603832259a31a38f
Matt Hixon <mhixon@bronco.cis.ksu.edu>
2016-05-03 02:26:55 -0500

    final push for cis308

--- Lab7_CockroachBeeper/lab7.c
+++ Lab7_CockroachBeeper/lab7.c
@@ -1,3 +1,5 @@
+//Lab 7: Cockroach Beeper
+//Author: Matt Hixon
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +9,10 @@
 
 #define SLEEP_TIME   5
 
+//global variables for pids
+static pid_t parent = 0;
+static pid_t child = 0;
+
 static char *G_signames[] = {"NONE",
                              "SIGHUP",
                              "SIGINT",
@@ -14,116 +20,80 @@ static char *G_signames[] = {"NONE",
                              "SIGILL",
                              "SIGTRAP",
                              "SIGABRT",
-                             "SIGEMT",
+                             "SIGBUS",
                              "SIGFPE",
                              "SIGKILL",
-                             "SIGBUS",
+                             "SIGUSR1",
                              "SIGSEGV",
-                             "SIGSYS",
+                             "SIGUSR2",
                              "SIGPIPE",
                              "SIGALRM",
                              "SIGTERM",
-                             "SIGURG",
+                             "SIGSTKFLT",
+                             "SIGCHLD",
+                             "SIGCONT",
                              "SIGSTOP",
                              "SIGTSTP",
-                             "SIGCONT",
-                             "SIGCHLD",
                              "SIGTTIN",
                              "SIGTTOU",
-                             "SIGIO",
+                             "SIGYRG",
                              "SIGXCPU",
                              "SIGXFSZ",
                              "SIGVTALRM",
                              "SIGPROF",
                              "SIGWINCH",
-                             "SIGINFO",
-                             "SIGUSR1",
-                             "SIGUSR2",
+                             "SIGIO",
+                             "SIGPWR",
+                             "SIGSYS",
                              NULL};
 
-void handler(int sig) {
-	printf("We made it to the handler");
-	if(sig == 17){
-		//SIGCHILD recieved
-		//make new child
-		getpid());
-		parentBeeper();
-	}
 
-	if(sig == 15){
-		//SIGTERM
-		printf("SIGTERM recieved to %d ", getpid());
-		if(getpid() == 0){
-			
-			kill(getpid(), SIGKILL);
-			kill(getppid(), SIGKILL);
-		} else{
-			
-			kill(getppid(), SIGKILL);
-			kill(getpid(), SIGKILL);
-		}
-		
-	}
-
-    if (sig > 0 && sig < 32) {
-       printf("%d has received signal %d (%s)\n",
-               getpid(),
-               sig, G_signames[sig]);
-    } else {
-       printf("%d Invalid signal %d\n",
-              getpid(),
-              sig);
-    }
-}
-
-void parentBeeper(){
-	int        nap = SLEEP_TIME;
-    time_t     now;
-    struct tm *t;
-    int        remains = 0;
-    int        sig;
-    char       whoami[50];
-    pid_t      pid;
-	
-	
+void forkMe(){
+	//set global variables every fork
+	pid_t pid;
 	if ((pid = fork()) == -1){
+		//error forking
 		perror("fork()");
 		exit(1);
+	} else if(pid == 0){
+		//child
+		parent = getppid();
+		child = getpid();
+	}else{
+		//parent
+		parent = getpid();
+		child = pid;
 	}
-	for (sig = 1; sig < 32; sig++) {
-      (void)signal(sig, handler);
-    }
-	if(pid == 0){
-		//you are the child
-		childBeeper();
-	} else{
-		//you are the parent start beeping
-		while (1) {
-				   if (remains == 0) {
-					  now = time(NULL);
-					  t = localtime(&now);
-					  printf("PARENT: %d %02d:%02d:%02d beep!\n",
-						      getpid(),
-						      t->tm_hour, t->tm_min, t->tm_sec);
-					  remains = sleep(nap);
-					} else {
-					  remains = sleep(remains - 1);
-					}
-				}
-			}
 }
 
-void childBeeper(){
-//you are child, check for parent
-	if(getppid() == 31410){
-		parentBeeper();
-	} else{
-		usleep(500);
-		//printf("CHJILD : %d", getpid());
-		childBeeper();
-	}
-	
+void handler(int sig) {
+	//handle exceptions to act as cockroach
+	if(sig == 17){
+		//SIGCHILD recieved, fork another process
+		printf("%d has received signal %d (%s)\n", getpid(), sig, G_signames[sig]);
+		forkMe();
+	} else if(sig == 15){
+		//SIGTERM recieved, kill everything
+		printf("%d has received signal %d (%s)\n", getpid(), sig, G_signames[sig]);
+
+		if(getpid() == child){
+			kill(parent, SIGKILL);
+			kill(child, SIGKILL);
+		} else {
+			kill(child, SIGKILL);
+			kill(parent, SIGKILL);
+		}
+		
+	} else if (sig > 0 && sig < 32) {
+		//display any other signal
+       printf("%d has received signal %d (%s)\n", getpid(), sig, G_signames[sig]);
+    } else {
+		//error recieving signal
+       printf("%d has received signal %d (%s)\n", getpid(), sig, G_signames[sig]);
+    }
 }
+
+
 
 int main() {
     int        nap = SLEEP_TIME;
@@ -135,10 +105,34 @@ int main() {
     pid_t      pid;
 	
 	//set handler to handle all signals
-    
+    for (sig = 1; sig < 32; sig++) {
+      (void)signal(sig, handler);
+    }
+	//fork process
+	forkMe();
+	while(1){
+		if(getpid() == child){
+			//Child
+			usleep(500);
+			//kill returns -1 if parent is dead
+			if(kill(parent, 0) == -1){
+				//Parent dead, fork
+				forkMe();
+			}
+		} else{
+			//Parent
+		   	if (remains == 0) {
+			  now = time(NULL);
+			  t = localtime(&now);
+			  printf("PARENT: %d %02d:%02d:%02d beep!\n",
+					  getpid(),
+					  t->tm_hour, t->tm_min, t->tm_sec);
+			  remains = sleep(nap);
+			} else {
+			  remains = sleep(remains - 1);
+			}
+		}
 
-	parentBeeper();	
-
-	//both child and parent run this
+	}
     return 0;
 }
